fab1be57c25f86234b745f2f64529e924c18cea2
Gordana Radulović <gordana.r13@gmail.com>
2017-02-07 12:10:49 +0100

    skrito stevilo

---
+++ skrito_stevilo.py
@@ -0,0 +1,7 @@
+skrito_stevilo = "12"
+ugani = raw_input("Ugani skrito stevilo:")
+while ugani != "12":
+    skrito_stevilo = raw_input("Poskusi se enkrat:")
+    if skrito_stevilo == "12":
+        print("Pravilno")
+        break
